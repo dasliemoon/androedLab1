f69c212f2f675fdce017a5a1817b8ead6c5bc8ac
dasliemoon <85513582+dasliemoon@users.noreply.github.com>
2023-02-28 13:29:17 +0500

    Fixed lab1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import com.example.myapplication.databinding.ActivityMainBinding
 
 
-class  MainActivity : AppCompatActivity() {
-    private lateinit var bindingClass : ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    private lateinit var bindingClass: ActivityMainBinding
 
 
     override fun onCreate(s: Bundle?) {
@@ -15,15 +14,7 @@ class  MainActivity : AppCompatActivity() {
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
         bindingClass.showButton.setOnClickListener() {
-
-            if (bindingClass.showButton.text == "Отобразить") {
-                bindingClass.showButton.text = "Скрыть"
-                bindingClass.textView.isVisible = true
-            }
-            else {
-                bindingClass.showButton.text = "Отобразить"
-                bindingClass.textView.isVisible = false
-            }
+            bindingClass.textView.text = bindingClass.EnterText.text.toString()
         }
     }
 }
